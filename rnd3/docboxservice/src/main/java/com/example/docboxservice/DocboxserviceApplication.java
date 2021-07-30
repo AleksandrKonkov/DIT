d6b32cebbf9354fd87950f38f7de121090f4b539
AleksandrKonkov <aleksnk@gmail.com>
2021-07-30 10:25:11 +0300

    task 3 start

---
+++ rnd3/docboxservice/src/main/java/com/example/docboxservice/DocboxserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.docboxservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocboxserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( DocboxserviceApplication.class, args );
+    }
+
+}
---
+++ rnd3/docboxservice/src/test/java/com/example/docboxservice/DocboxserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docboxservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocboxserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
