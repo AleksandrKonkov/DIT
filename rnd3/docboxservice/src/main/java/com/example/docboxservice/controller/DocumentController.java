fc743181547e57ccd6ff0a056187ce07fc110b50
AleksandrKonkov <aleksnk@gmail.com>
2021-08-03 11:51:14 +0300

    restcontroller

--- rnd3/docboxservice/src/main/java/com/example/docboxservice/controller/DocumentController.java
+++ rnd3/docboxservice/src/main/java/com/example/docboxservice/controller/DocumentController.java
@@ -12,9 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
-
-@Controller
-
+@RestController
 public class DocumentController {
 
     private final DocumentService documentService;
